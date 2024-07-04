024606ed74dfa3dd360505a81be6ba46711f9c0c
Nagendra <122201010@smail.iitpkd.ac.in>
2024-07-04 10:17:15 +0530

    "updated"

--- openCV/demo.py
+++ openCV/demo.py
@@ -23,6 +23,27 @@ def shape_selection(event, x, y, flags, param):
       # draw a rectangle
       cv2.rectangle(image, point[0], point[1], (0, 255, 0), 2)
      #   image.show()
+def write_text():
+   # font 
+   font = cv2.FONT_HERSHEY_SIMPLEX 
+
+   # org 
+   org = point[0] 
+
+   # fontScale 
+   fontScale = 1
+
+   # Blue color in BGR 
+   color = (255, 0, 0) 
+
+   # Line thickness of 2 px 
+   thickness = 2
+
+   # Using cv2.putText() method 
+   image = cv2.putText(image, 'OpenCV', org, font,  
+                  fontScale, color, thickness, cv2.LINE_AA) 
+   cv2.imshow("Text entered", image)
+   cv2.waitKey(0)
   
 image_path=r"D:\COLLEGE\B.Tech 5th SEM\yacc_project\certificate_generator\certificate_gen2.jpg"
 # load the image
@@ -33,35 +54,16 @@ cv2.namedWindow("image")
 names=['AA_BB_CC_DD_EE1', 'AA_BB_CC_DD_EE2', 'AA_BB_CC_DD_EE3', 'AA_BB_CC_DD_EE4', 'AA_BB_CC_DD_EE5']
   
 while True:
-    cv2.imshow("image", image)
-    cv2.setMouseCallback("image", shape_selection)
-    # display the image and wait for a keypress
-    key = cv2.waitKey(1) & 0xFF
-  
-    # if the 'c' key is pressed
-    if key == ord("c"):
-        # font 
-        font = cv2.FONT_HERSHEY_SIMPLEX 
-        
-        # org 
-        org = point[0] 
-        
-        # fontScale 
-        fontScale = 1
-        
-        # Blue color in BGR 
-        color = (255, 0, 0) 
-        
-        # Line thickness of 2 px 
-        thickness = 2
-        
-        # Using cv2.putText() method 
-        image = cv2.putText(image, 'OpenCV', org, font,  
-                        fontScale, color, thickness, cv2.LINE_AA) 
-        cv2.imshow("Text entered", image)
-        cv2.waitKey(0)
-            
-        # close all open windows
-        cv2.destroyAllWindows()
-        image=clone.copy()
-        continue
+   cv2.imshow("image", image)
+   cv2.setMouseCallback("image", shape_selection)
+   # display the image and wait for a keypress
+   key = cv2.waitKey(1) & 0xFF
+
+   # if the 'c' key is pressed
+   if key == ord("c"):
+      write_text
+                  
+   # close all open windows
+   cv2.destroyAllWindows()
+   image=clone.copy()
+   continue
