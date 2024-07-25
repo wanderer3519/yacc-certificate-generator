7a270957af2b0a11d4c1cc9338686477b41c9419
Krish_Raziel19 <112201013@smail.iitpkd.ac.in>
2024-07-25 23:57:10 +0530

    Added data handling part

---
+++ read_csv/Names.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# file_name = './read_csv/Data-for-Certificate.xlsx'
+
+def Names(file_name):
+    # Read the Excel file
+    data = pd.read_excel(file_name)
+    cols = list(data.columns)
+    d = data.to_dict()
+
+
+    names = [d[cols[1]][i] for i in d[cols[1]]]
+    # print(names)
+    return names
