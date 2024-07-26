49bac297875febefc9773de3225f942eb9b49019
Murali47k <murali47karthick@gmail.com>
2024-07-26 15:07:28 +0530

    name addition function commit

--- openCV/main.py
+++ openCV/main.py
@@ -1,16 +1,27 @@
 # from signature_addition import ...
-# from openCV_text_addition import ...
+from openCV_text_addition.certificate_openCv import add_name
 
 # main template
+import cv2
+image = cv2.imread(r'mini projects\certificate_gen\certificate.jpg')
 
-def main(is_sign_added, is_watermark_added, data, coordinates):
+def main(is_sign_added, is_watermark_added, names , startX,startY,endX,endY):
 
-    if not is_sign_added:
-        pass
-        # do sth
+    for name in names:
+        if not is_sign_added:
+            pass
+            # do sth
 
-    if not is_watermark_added:
-        pass
-        #do sth
-    pass
+        if not is_watermark_added:
+            pass
+            #do sth
+    
+        else:
+            add_name(image,name,startX,startY,endX,endY)
+
+            
+        
+
+
+
 
--- openCV/openCV_text_addition/certificate_openCv.py
+++ openCV/openCV_text_addition/certificate_openCv.py
@@ -1,16 +1,7 @@
 import cv2
-# Read the image from file
-image = cv2.imread(r'mini projects\certificate_gen\certificate.jpg')
-# Check if the image was loaded successfully
-if image is None:
-    print("Error: Could not load image.")
-else:
-    width = 600
-    height = 400 
-    image = cv2.resize(image, (width,height))
 
-    name = "Your Name"
-    name_loc_height=218
+def add_name(image,name,startX,startY,endX,endY):
+    name_loc_height=startY
     font_scale = 1
     thickness = 1
 
@@ -25,12 +16,7 @@ else:
     print(f"Text width: {text_width}")
     print(f"Text height: {total_height}")
 
-    name_loc_width = width//2 - (text_width//2)
+    name_loc_width = (startX + endX)//2 - (text_width//2)
 
     text = cv2.putText(image,name,(name_loc_width,name_loc_height),font,font_scale,(0,0,255),thickness,cv2.LINE_AA)
 
-    # Display the image in a window
-    cv2.imshow('Image', image)
-    # Wait for a key press and close the image window
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
