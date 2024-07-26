7929ec03c19d5e8d0ded91f1f33b2e4d0a124642
Shashank1304 <112201014@smail.iitpkd.ac.in>
2024-07-26 16:00:17 +0530

    resolving merge

--- openCV/main.py
+++ openCV/main.py
@@ -1,15 +1,52 @@
-# from signature_addition import ...
+
 from openCV_text_addition.certificate_openCv import add_name
+import openCV as cv2
+import numpy as np
+from signature_addition.certificate_gen import add_signature
 
 # main template
-import cv2
 image = cv2.imread(r'mini projects\certificate_gen\certificate.jpg')
 
 def main(is_sign_added, is_watermark_added, names , startX, startY, endX, endY):
 
     for name in names:
-        if not is_sign_added:
-            pass
+        if not is_sign_added:   # To do: Once the input for coordiates is available, we will use them as x_offset and y_offset.
+            certificate_path = '../Assets/certificate.jpeg'
+            signature_path1 = '../Assets/img.png'
+            signature_path2 = '../Assets/img1.png'
+            
+            # Read the base image using path
+            base_image = cv2.imread(certificate_path)
+            if base_image is None:
+                print(f"Error: Unable to load image from given path: {certificate_path}")
+                exit()
+
+            # Read the first signature image using path
+            signature_image1 = cv2.imread(signature_path1, cv2.IMREAD_UNCHANGED)
+            if signature_image1 is None:
+                print(f"Error: Unable to load image from given path: {signature_path1}")
+                exit()
+
+            # Read the second signature image using path
+            signature_image2 = cv2.imread(signature_path2, cv2.IMREAD_UNCHANGED)
+            if signature_image2 is None:
+                print(f"Error: Unable to load image from given path: {signature_path2}")
+                exit()
+                
+            # Call add_signature to add the first signature to the base image.
+            base_image = add_signature(base_image = base_image, signature_image = signature_image1, x_offset = 150, y_offset = 602, scale_w = 0.3, scale_h = 0.1)
+            
+            # Calculate y_offset for the second signature
+            y_offset_second_signature = base_image.shape[0] - int(base_image.shape[0] * 0.1) - 210
+
+            # Call add_signature to add the second signature to the base image 
+            base_image = add_signature(base_image = base_image, signature_image = signature_image2, x_offset = 720, y_offset = y_offset_second_signature, scale_w = 0.3,scale_h = 0.1)
+
+            # Save or display the final image
+            cv2.imwrite('final_image_with_two_signatures.jpg', base_image)
+            cv2.imshow('Final Image with Two Signatures', base_image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
             # do sth
 
         if not is_watermark_added:
