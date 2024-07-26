bc6ab495843971cf3add60305aaecd0aad7312c4
Bharath <kvsbharath296@gmail.com>
2024-07-26 17:11:12 +0530

    Calling main function from opencv

--- flask_linking/app.py
+++ flask_linking/app.py
@@ -1,4 +1,8 @@
-from flask import Flask,render_template, request ,redirect
+from flask import Flask,render_template, request ,redirect, url_for
+import sys
+sys.path.append('..')
+import openCV.main as processfn
+
 
 app = Flask(__name__,template_folder="frontend") 
 
@@ -10,23 +14,27 @@ def home():
 def StoringInFile(): 
     data = request.json
     with open("frontend_data.txt", "a") as fh:
-        startX = str(data['startX'])
-        startY = str(data['startY'])
-        endX = str(data['endX'])
-        endY = str(data['endY'])
-        imagepath = '../'
+        global startX, startY, endX, endY
+        startX = int(data['startX'])
+        startY = int(data['startY'])
+        endX = int(data['endX'])
+        endY = int(data['endY'])
+        imagepath = '../Assets/"Certificate.jpeg'
         data = f"startX: {startX}, startY: {startY}, endX: {endX}, endY: {endY}\n"
         fh.write(data)
     return redirect('rectangle.html') 
 # call openCV
 # main(true, true, data, coords)
 
+@app.route("/processimage")
 def processImage():
-    pass
-    NAMES = ['1', '2']
-    is_sign_added = True
-    is_watermark_added = False
-
     '''
         Calls the main in openCV
     '''
+    NAMES = ['1', '2']
+    is_sign_added = True
+    is_watermark_added = False
+    finimg = processfn.main(is_sign_added, is_watermark_added, NAMES, startX, startY, endX, endY)
+    return redirect('/')
+
+
