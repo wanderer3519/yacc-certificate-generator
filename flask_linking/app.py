56cd5cc9beb25e3224bbc04cc7cb5cd4e0b1159d
Vishesh <112301037@smail.iitpkd.ac.in>
2024-08-01 03:33:59 +0530

    Making the ffile upload in flask

--- flask_linking/app.py
+++ flask_linking/app.py
@@ -1,43 +1,64 @@
-from flask import Flask,render_template, request ,redirect, url_for
-from werkzeug.utils import secure_filename
+from flask import Flask, render_template, request, redirect
 import sys
-sys.path.append('..')
+import os  
+
+sys.path.append('..')  
 import openCV.main as processfn
 from read_csv.Names import Names
 
 data_path = '../Assets/Data-for-Certificate.xlsx'
 image = '../Assets/Certificate.jpeg'
-app = Flask(__name__,template_folder="frontend") 
+app = Flask(__name__, template_folder="frontend")
+i = 0
 
+startX=startY=endX=endY=""
 @app.route("/")
 def home():
     return render_template('rectangle.html')
 
-@app.route("/storeinfile", methods=['POST']) 
-def StoringInFile(): 
-    data = request.get_json()
+@app.route("/storefile", methods=['POST'])
+def storefile():
+    global i
+    file = request.files['imageLoader']
+    image_filename = f"image{i}.jpeg"
+    image_dir = "uploads"  
+    os.makedirs(image_dir, exist_ok=True)  
+    image_path = os.path.join(image_dir, image_filename)  
+    i += 1
+    file.save(image_path)
+
     with open("frontend_data.txt", "a") as fh:
-        global startX, startY, endX, endY, imagepath
-        startX = int(data['startX'])
-        startY = int(data['startY'])
-        endX = int(data['endX'])
-        endY = int(data['endY'])
-        imagepath = data['image']
-        data = f"startX: {startX}, startY: {startY}, endX: {endX},endY: {endY} image: {imagepath}"
-        fh.write(data)
-    return redirect('rectangle.html') 
-# call openCV
-# main(true, true, data, coords)
+        fh.write(f"imagePath: {image_path}\n")
+
+    return redirect('/')
+
+
+@app.route("/storeinfile", methods=['POST'])
+def storeinfile():
+    data = request.get_json()
+    startX = int(data['startX'])
+    startY = int(data['startY'])
+    endX = int(data['endX'])
+    endY = int(data['endY'])
+
+    with open("frontend_data.txt", "a") as fh:
+        fh.write(f"startX: {startX}, startY: {startY}, endX: {endX}, endY: {endY} ")
+
+    return redirect('/')
+    
+
 
 @app.route("/processimage")
 def processImage():
-    '''
-        Calls the main in openCV
-    '''
+    """
+    Calls the main in openCV
+    """
     NAMES = Names(data_path)
     is_sign_added = True
     is_watermark_added = True
-    finimg = processfn.main(image, is_sign_added, is_watermark_added, NAMES, startX, startY, endX, endY)
+    image_path = "path/to/your/image.png"  
+    finimg = processfn.main(image_path, is_sign_added, is_watermark_added, NAMES, startX, startY, endX, endY)
     return redirect('/')
+ 
 
 
